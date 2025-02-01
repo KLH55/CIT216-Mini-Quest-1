6c6a969007009e4d3d796a2432f6cfb2012c7213
Kristopher Herbert <klh55@pct.edu>
2025-01-31 20:38:02 -0500

    Roll-A-Ball Game

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+   
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        Debug.Log(offset);
+    }
+
+    
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+        Debug.Log(transform.position);
+    }
+}
